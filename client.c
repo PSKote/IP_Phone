a4a9e8363a614f269823ab2a9aec193c7d150ec2
PSKote <prajwalsharmakote@gmail.com>
2017-03-04 18:21:51 +0530

    It connects to server and sends the audio

--- client.c
+++ client.c
@@ -17,68 +17,89 @@
 #include <pulse/error.h>
 #include <pulse/gccmacro.h>
 
+#include <stdio.h>	/* standard I/O routines.               */
+#include <stdlib.h>	/* rand(), macros, etc.                 */
+#include <errno.h>	/* defines integer value for errorno	*/
+#include <string.h>	/* handling string operations 		*/
+#include <sys/types.h>	/* various type definitions.            */
+#include <sys/socket.h>	/* sockaddr structure			*/
+#include <netinet/in.h>	/* htons()				*/
+#include <arpa/inet.h>	/* defines internet operation		*/
+#include <unistd.h>	/* fork()                               */
+#include <signal.h>	/* handle signals			*/
+
 #define BUFSIZE 1024
 
-/* A simple routine calling UNIX write() in a loop */
-static ssize_t loop_write(int fd, const void*data, size_t size) {
-    ssize_t ret = 0;
+int main(int argc, char*argv[]) 
+{
 
-    while (size > 0) {
-        ssize_t r;
 
-        if ((r = write(fd, data, size)) < 0)
-            return r;
+	int sock, t, len;
+	struct sockaddr_in client;
+	//char str[MAXDATASIZE];		/* buffer for message */
 
-        if (r == 0)
-            break;
+	/* creating socket stream */
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
+	{
+		perror("socket");
+		exit(1);
+	}
+	printf("Welcome to My Voice Call\n");
+	client.sin_family=AF_INET;
+        client.sin_port=htons(atoi(argv[2]));		/* port address */
+	client.sin_addr.s_addr=inet_addr(argv[1]);	/* IP address   */
 
-        ret += r;
-        data = (const uint8_t*) data + r;
-        size -= (size_t) r;
-    }
+	if (connect(sock, (struct sockaddr *)&client, sizeof(client)) == -1) 
+	{
+		perror("connect");
+		exit(1);
+	}
 
-    return ret;
-}
 
-int main(int argc, char*argv[]) {
-    /* The sample type to use */
-    static const pa_sample_spec ss = {
-        .format = PA_SAMPLE_S16LE,
-        .rate = 44100,
-        .channels = 2
-    };
-    pa_simple *s = NULL;
-    int ret = 1;
-    int error;
-
-    /* Create the recording stream */
-    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
-        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
-        goto finish;
-    }
-
-    for (;;) {
+   	/* The sample type to use */
+    	static const pa_sample_spec ss = {
+        	.format = PA_SAMPLE_S16LE,
+        	.rate = 44100,
+        	.channels = 2
+    	};
+    	pa_simple *s = NULL;
+    	int ret = 1;
+    	int error;
         uint8_t buf[BUFSIZE];
 
-        /* Record some data ... */
-        if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
-            fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
-            goto finish;
-        }
+    	/* Create the recording stream */
+    	if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) 
+	{
+        	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        	goto finish;
+    	}
 
-        /* And write it to STDOUT */
-        if (loop_write(STDOUT_FILENO, buf, sizeof(buf)) != sizeof(buf)) {
-            fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
-            goto finish;
-        }
-    }
 
-    ret = 0;
+	printf("Start your conversation with %s\n", argv[1]);
 
-finish:
+    	for (;;) 
+	{
 
-    if (s)
-        pa_simple_free(s);
+        	/* Record some data ... */
+        	if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) 
+		{
+            		fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+            		goto finish;
+        	}
 
-    return ret;
+		if (send(sock, buf, strlen(buf), 0) == -1) 
+		{
+			perror("send");
+			exit(1);
+		}
+    	}
+
+    	ret = 0;
+
+	finish:
+	close(sock);
+    	if (s)
+        	pa_simple_free(s);
+
+    	return ret;
 }
