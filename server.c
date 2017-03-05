f8e487dba5a8f45d1550cd2cfa12415b3974f1aa
PSKote <prajwalsharmakote@gmail.com>
2017-03-05 21:06:55 +0530

    Plays audio sent by client. Echo effect is present. Picks up noise. Tested with internal mic and Loopback

--- server.c
+++ server.c
@@ -31,39 +31,49 @@
 
 #define BUFSIZE 1024
 
-#define BACKLOG 5	/* clients waiting is queue		*/
+#define BACKLOG 10	/* clients waiting is queue		*/
 
-/* A simple routine calling UNIX write() in a loop */
-static ssize_t loop_write(int fd, const void*data, size_t size) {
-    ssize_t ret = 0;
+int s2;
 
-    while (size > 0) {
-        ssize_t r;
-
-        if ((r = write(fd, data, size)) < 0)
-            return r;
-
-        if (r == 0)
-            break;
-
-        ret += r;
-        data = (const uint8_t*) data + r;
-        size -= (size_t) r;
-    }
-
-    return ret;
+void my_handler_for_sigint(int signumber)//signal handler
+{
+  	char ans[2];
+  	if (signumber == SIGINT)
+  	{
+    		printf("received SIGINT\n");
+    		printf("Program received a CTRL-C\n");
+    		printf("Terminate Y/N : "); 
+    		scanf("%s", ans);
+    		if (strcmp(ans,"Y") == 0)
+    		{
+       			printf("Exiting ....\n"); 
+			close(s2);      
+			exit(0); 
+    		}
+    		else
+    		{
+       			printf("Continung ..\n");
+    		}
+  	}
 }
-
 
 int main(int argc, char*argv[]) {
 
 
-	int sock, s2, t, len;
+	int sock, t, len;
 	struct sockaddr_in server, client;
 	struct sigaction sa;
 
-        uint8_t buf[BUFSIZE];
-        ssize_t r;
+    	/* The Sample format to use */
+    	static const pa_sample_spec ss = {
+        	.format = PA_SAMPLE_S16LE,
+        	.rate = 44100,
+        	.channels = 2
+    	};
+
+    	pa_simple *saa = NULL;
+    	int ret = 1;
+    	int error;
 
 	/* creating socket stream */
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
@@ -89,19 +99,12 @@ int main(int argc, char*argv[]) {
 	}
 
 
-    	/* The Sample format to use */
-    	static const pa_sample_spec ss = {
-        	.format = PA_SAMPLE_S16LE,
-        	.rate = 44100,
-        	.channels = 2
-    	};
-
-    	pa_simple *s = NULL;
-    	int ret = 1;
-    	int error;
+//registering the signal handler
+if (signal(SIGINT, my_handler_for_sigint) == SIG_ERR)
+      printf("\ncan't catch SIGINT\n");
 
     	/* Create a new playback stream */
-    	if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) 
+    	if (!(saa = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error))) 
 	{
         	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
         	goto finish;
@@ -109,7 +112,7 @@ int main(int argc, char*argv[]) {
 
 	for(;;) 
 	{
-		int done, n;
+		int n;
 		printf("Waiting for a connection\n");
 
 		/* accept connection fron client */
@@ -119,60 +122,54 @@ int main(int argc, char*argv[]) {
 			exit(1);
 		}
 		printf("Connected. Start conversation\n");
+	
+		for(;;)
+		{
+		
+        		uint8_t bufs[BUFSIZE];
+        		ssize_t r;
 
-		/* receiving message from client */
-		done = 0;
-		do 
-		  {
-			n = recv(s2, buf, BUFSIZE, 0);
-			if (n <= 0) 
+			#if 0
+        		pa_usec_t latency;
+
+        		if ((latency = pa_simple_get_latency(saa, &error)) == (pa_usec_t) -1) {
+            			fprintf(stderr, __FILE__": pa_simple_get_latency() failed: %s\n", pa_strerror(error));
+            			goto finish;
+        		}
+
+        		fprintf(stderr, "%0.0f usec    \r", (float)latency);
+			#endif
+
+			/* receiving message from client */
+/*			n = recv(s2, bufs, sizeof(bufs), 0);
+			if(n == -1)
 			{
-				if (n < 0) 
-					perror("recv");
-				done = 1;
+        			perror("recv");
+       				exit(1);
 			}
-			if (!done)
-			{
-				printf("Receiving\n");
-
-				/* And write it to STDIN */
-        			if (loop_write(STDIN_FILENO, buf, sizeof(buf)) != sizeof(buf)) 
-				{
-            				fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
-            				goto finish;
-        			}
-			}
-
-		  } 
-		while (!done);
-		close(s2);
-
-//		for (;;) 
-//		{
-			printf("playing\n");
+*/
 
         		/* Read some data ... */
-	       		if ((r = read(STDIN_FILENO, buf, sizeof(buf))) <= 0) 
+        		if ((r = read(s2, bufs, sizeof(bufs))) <= 0) 
 			{
             			if (r == 0) /* EOF */
-	               			break;
+                			break;
 
             			fprintf(stderr, __FILE__": read() failed: %s\n", strerror(errno));
             			goto finish;
-	       		}
+       	 		}
 
-        		/* ... and play it */
-        		if (pa_simple_write(s, buf, (size_t) r, &error) < 0) 
+        		if (pa_simple_write(saa, bufs, (size_t) r, &error) < 0) 
 			{
             			fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
             			goto finish;
         		}
-//    		}
+		}
 	}
 
 
     	/* Make sure that every single sample was played */
-    	if (pa_simple_drain(s, &error) < 0) 
+    	if (pa_simple_drain(saa, &error) < 0) 
 	{
         	fprintf(stderr, __FILE__": pa_simple_drain() failed: %s\n", pa_strerror(error));
         	goto finish;
@@ -181,8 +178,9 @@ int main(int argc, char*argv[]) {
     	ret = 0;
 
 	finish:
-    	if (s)
-        	pa_simple_free(s);
+    	if (saa)
+        	pa_simple_free(saa);
 
     	return ret;
+
 }
