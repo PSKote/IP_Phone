430974786282de55e1ad71e8551b20b9ee8ae79c
PSKote <prajwalsharmakote@gmail.com>
2017-03-05 22:40:49 +0530

    Internal mic and Loopback tested

--- client.c
+++ client.c
@@ -1,21 +1,17 @@
-/***
- client.c -----
- save the recorded audio to a file. 
- ./client > <filename>
-***/
+/*
+** client.c -----
+** Sends the audio to server to which it is connected. 
+** ./client <IP_address> <port_number>
+*/
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-
-#include <pulse/simple.h>
-#include <pulse/error.h>
-#include <pulse/gccmacro.h>
+#include <fcntl.h>		/* file control options 			*/
+#include <pulse/simple.h>	/* synchronous playback and recording API */
+#include <pulse/error.h>	/* error management */
+#include <pulse/gccmacro.h>	/* GCC attribute macros */
 
 #include <stdio.h>	/* standard I/O routines.               */
 #include <stdlib.h>	/* rand(), macros, etc.                 */
@@ -28,11 +24,13 @@
 #include <unistd.h>	/* fork()                               */
 #include <signal.h>	/* handle signals			*/
 
-#define BUFSIZE 1024
+#define BUFSIZE 1024	/* buffer size for data 		*/
 
-int sock;
+int sock;		/* socket 				*/
 
-void my_handler_for_sigint(int signumber)//signal handler
+/* signal Handler */
+void 
+my_handler_for_sigint(int signumber)
 {
   	char ans[2];
   	if (signumber == SIGINT)
@@ -55,22 +53,24 @@ void my_handler_for_sigint(int signumber)//signal handler
 }
 
 /* A simple routine calling UNIX write() in a loop */
-static ssize_t loop_write(int fd, const void*data, size_t size) {
-    ssize_t ret = 0;
+static ssize_t loop_write(int fd, const void*data, size_t size) 
+{
+    	ssize_t ret = 0;
 
-    while (size > 0) {
-        ssize_t r;
+    	while (size > 0) 
+	{
+        	ssize_t r;
 
-        if ((r = write(fd, data, size)) < 0)
-            return r;
+        	if ((r = write(fd, data, size)) < 0)
+            		return r;
 
-        if (r == 0)
-            break;
+        	if (r == 0)
+            		break;
 
-        ret += r;
-        data = (const uint8_t*) data + r;
-        size -= (size_t) r;
-    }
+        	ret += r;
+        	data = (const uint8_t*) data + r;
+        	size -= (size_t) r;
+    	}	
 
     return ret;
 }
@@ -78,13 +78,12 @@ static ssize_t loop_write(int fd, const void*data, size_t size) {
 int main(int argc, char*argv[]) 
 {
 
-     /* Registering the Signal handler */
-  if (signal(SIGINT, my_handler_for_sigint) == SIG_ERR)
-      printf("\ncan't catch SIGINT\n");
+     	/* registering the Signal handler */
+  	if (signal(SIGINT, my_handler_for_sigint) == SIG_ERR)
+      		printf("\ncan't catch SIGINT\n");
 
 	int t, len;
 	struct sockaddr_in client;
-	//char str[MAXDATASIZE];		/* buffer for message */
 
 	/* creating socket stream */
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
@@ -104,7 +103,7 @@ int main(int argc, char*argv[])
 	}
 
 
-   	/* The sample type to use */
+   	/* the sample type to use */
     	static const pa_sample_spec ss = {
         	.format = PA_SAMPLE_S16LE,
         	.rate = 44100,
@@ -114,7 +113,7 @@ int main(int argc, char*argv[])
     	int ret = 1;
     	int error;
 
-    	/* Create the recording stream */
+    	/* create the recording stream */
     	if (!(sc = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) 
 	{
         	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
@@ -128,13 +127,14 @@ int main(int argc, char*argv[])
 	{
 	        uint8_t buf[BUFSIZE];
 
-        	/* Record some data ... */
+        	/* record some data */
         	if (pa_simple_read(sc, buf, sizeof(buf), &error) < 0) 
 		{
             		fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
             		goto finish;
         	}
 
+		/* writing audio data to socket */
 		if (loop_write(sock, buf, sizeof(buf)) != sizeof(buf)) 
 		{
             		fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
@@ -145,9 +145,9 @@ int main(int argc, char*argv[])
     	ret = 0;
 
 	finish:
+	/* close and free the connection to the server */
     	if (sc)
         	pa_simple_free(sc);
 
     	return ret;
-	close(sock);
 }
