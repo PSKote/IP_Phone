68cc585c36969cd6f6ecad4ad0d07b2859e658a2
Prajwal Sharma K <prajwalsharmakote@gmail.com>
2017-03-24 00:57:41 +0530

    Add files via upload
    With Periodic Scheduler

--- client.c
+++ client.c
@@ -1,6 +1,7 @@
 /*
 ** client.c -----
 ** Sends the audio to server to which it is connected. 
+** Uses Periodic Scheduling to sample the audio. Sampling rate is 1sec.
 ** ./client <IP_address> <port_number>
 */
 
@@ -23,34 +24,21 @@
 #include <arpa/inet.h>	/* defines internet operation		*/
 #include <unistd.h>	/* fork()                               */
 #include <signal.h>	/* handle signals			*/
+#include <sys/time.h>	/* timer				*/
 
 #define BUFSIZE 1024	/* buffer size for data 		*/
 
 int sock;		/* socket 				*/
 
-/* signal Handler */
-void 
-my_handler_for_sigint(int signumber)
-{
-  	char ans[2];
-  	if (signumber == SIGINT)
-  	{
-    		printf("received SIGINT\n");
-    		printf("Program received a CTRL-C\n");
-    		printf("Terminate Y/N : "); 
-    		scanf("%s", ans);
-    		if (strcmp(ans,"Y") == 0)
-    		{
-       			printf("Exiting ....\n");
-       			close(sock);       
-       			exit(0); 
-    		}
-    		else
-    		{
-       			printf("Continung ..\n");
-    		}
-  	}
-}
+/* the sample type to use */
+static const pa_sample_spec ss = {
+	.format = PA_SAMPLE_S16LE,
+	.rate = 44100,
+	.channels = 2
+};
+pa_simple *sc = NULL;
+int ret = 1;
+int error;
 
 /* A simple routine calling UNIX write() in a loop */
 static ssize_t loop_write(int fd, const void*data, size_t size) 
@@ -75,12 +63,80 @@ static ssize_t loop_write(int fd, const void*data, size_t size)
     return ret;
 }
 
+void 
+periodic_task  (int signum)
+{
+	char ans[2];
+	uint8_t buf[BUFSIZE];
+	switch(signum)
+	{
+		case SIGINT:
+		  	if (signum == SIGINT)
+		  	{
+		    		printf("received SIGINT\n");
+		    		printf("Program received a CTRL-C\n");
+		    		printf("Terminate Y/N : "); 
+		    		scanf("%s", ans);
+		    		if (strcmp(ans,"Y") == 0)
+		    		{
+		       			printf("Exiting ....\n");
+		       			close(sock);       
+		       			exit(0); 
+		    		}
+		    		else
+		    		{
+		       			printf("Continung ..\n");
+		    		}
+		  	}
+			break;
+		case SIGVTALRM:
+			/* record some data */
+			if (pa_simple_read(sc, buf, sizeof(buf), &error) < 0) 
+			{
+			   	fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+			   	if (sc)
+					pa_simple_free(sc);
+				exit(1);
+			}
+
+			/* writing audio data to socket */
+			if (loop_write(sock, buf, sizeof(buf)) != sizeof(buf)) 
+			{
+			    	fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
+			   	if (sc)
+					pa_simple_free(sc);
+				exit(1);
+			}
+			break;
+	}
+}
+
 int main(int argc, char*argv[]) 
 {
 
+	struct sigaction sa;
+ 	struct itimerval timer;
+
+ 	/* Install periodic_task  as the signal handler for SIGVTALRM. */
+ 	memset (&sa, 0, sizeof (sa));
+ 	sa.sa_handler = &periodic_task ;
+ 	sigaction (SIGVTALRM, &sa, NULL);
+
+ 	/* Configure the timer to expire after 1 sec... */
+ 	timer.it_value.tv_sec = 0;
+ 	timer.it_value.tv_usec = 1000;
+
+ 	/* ... and every 1 sec after that. */
+ 	timer.it_interval.tv_sec = 0;
+ 	timer.it_interval.tv_usec = 1000;
+
+ 	/* Start a virtual timer. It counts down whenever this process is    executing. */
+ 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
      	/* registering the Signal handler */
-  	if (signal(SIGINT, my_handler_for_sigint) == SIG_ERR)
+  	if (signal(SIGINT, periodic_task) == SIG_ERR)
       		printf("\ncan't catch SIGINT\n");
+
 
 	int t, len;
 	struct sockaddr_in client;
@@ -103,15 +159,7 @@ int main(int argc, char*argv[])
 	}
 
 
-   	/* the sample type to use */
-    	static const pa_sample_spec ss = {
-        	.format = PA_SAMPLE_S16LE,
-        	.rate = 44100,
-        	.channels = 2
-    	};
-    	pa_simple *sc = NULL;
-    	int ret = 1;
-    	int error;
+
 
     	/* create the recording stream */
     	if (!(sc = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) 
@@ -125,21 +173,6 @@ int main(int argc, char*argv[])
 
     	for (;;) 
 	{
-	        uint8_t buf[BUFSIZE];
-
-        	/* record some data */
-        	if (pa_simple_read(sc, buf, sizeof(buf), &error) < 0) 
-		{
-            		fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
-            		goto finish;
-        	}
-
-		/* writing audio data to socket */
-		if (loop_write(sock, buf, sizeof(buf)) != sizeof(buf)) 
-		{
-            		fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
-           		goto finish;
-        	}
     	}
 
     	ret = 0;
