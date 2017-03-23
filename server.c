118481c6b61136a119313a3daa54c492777d13a1
Prajwal Sharma K <prajwalsharmakote@gmail.com>
2017-03-24 00:59:57 +0530

    With Periodic Scheduler

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 ** server.c -----
 ** When obtained connection from client.
 ** will reproduce the audio.
+** Uses Periodic Scheduling to reproduce the audio received. Sampling rate is 1sec.
 ** ./server <port_number>
 */
 
@@ -24,55 +25,109 @@
 #include <arpa/inet.h>	/* defines internet operation		*/
 #include <unistd.h>	/* fork()                               */
 #include <signal.h>	/* handle signals			*/
+#include <sys/time.h>	/* timer				*/
 
 #define BUFSIZE 1024	/* buffer size for data 		*/
 
 #define BACKLOG 10	/* clients waiting is queue		*/
 
 int s2;			/* socket 				*/
+int n;
+int sock;
 
-/* signal Handler */
+/* the Sample format to use */
+static const pa_sample_spec ss = {
+	.format = PA_SAMPLE_S16LE,
+	.rate = 44100,
+	.channels = 2
+};
+
+pa_simple *s = NULL;
+int ret = 1;
+int error;
+
 void 
-my_handler_for_sigint(int signumber)
+periodic_task  (int signum)
 {
-  	char ans[2];
-  	if (signumber == SIGINT)
-  	{
-    		printf("received SIGINT\n");
-    		printf("Program received a CTRL-C\n");
-    		printf("Terminate Y/N : "); 
-    		scanf("%s", ans);
-    		if (strcmp(ans,"Y") == 0)
-    		{
-       			printf("Exiting ....\n"); 
-			close(s2);      
-			exit(0); 
-    		}
-    		else
-    		{
-       			printf("Continung ..\n");
-    		}
-  	}
+	char ans[2];
+	uint8_t bufs[BUFSIZE];
+	ssize_t r;
+	switch(signum)
+	{
+		case SIGINT:
+		  	if (signum == SIGINT)
+		  	{
+		    		printf("received SIGINT\n");
+		    		printf("Program received a CTRL-C\n");
+		    		printf("Terminate Y/N : "); 
+		    		scanf("%s", ans);
+		    		if (strcmp(ans,"Y") == 0)
+		    		{
+		       			printf("Exiting ....\n");
+		       			close(sock);       
+		       			exit(0); 
+		    		}
+		    		else
+		    		{
+		       			printf("Continung ..\n");
+		    		}
+		  	}
+			break;
+		case SIGVTALRM:		
+			#if 0
+			pa_usec_t latency;
+
+			if ((latency = pa_simple_get_latency(s, &error)) == (pa_usec_t) -1) {
+				fprintf(stderr, __FILE__": pa_simple_get_latency() failed: %s\n", pa_strerror(error));
+				goto finish;
+			}
+
+			fprintf(stderr, "%0.0f usec    \r", (float)latency);
+			#endif
+
+			/* receiving message from client */
+			n = recv(s2, bufs, sizeof(bufs), 0);
+			if(n == -1)
+			{
+				perror("recv");
+				exit(1);
+			}
+
+			if (pa_simple_write(s, bufs, sizeof(bufs), &error) < 0) 
+			{
+				fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
+				if (s)
+					pa_simple_free(s);
+				exit(1);
+			}
+			break;
+	}
 }
 
 int main(int argc, char*argv[]) 
 {
 
 
-	int sock, t, len;
+	int t, len;
 	struct sockaddr_in server, client;
 	struct sigaction sa;
+ 	struct itimerval timer;
 
-    	/* the Sample format to use */
-    	static const pa_sample_spec ss = {
-        	.format = PA_SAMPLE_S16LE,
-        	.rate = 44100,
-        	.channels = 2
-    	};
+ 	/* Install periodic_task  as the signal handler for SIGVTALRM. */
+ 	memset (&sa, 0, sizeof (sa));
+ 	sa.sa_handler = &periodic_task ;
+ 	sigaction (SIGVTALRM, &sa, NULL);
 
-    	pa_simple *s = NULL;
-    	int ret = 1;
-    	int error;
+ 	/* Configure the timer to expire after 1 sec... */
+ 	timer.it_value.tv_sec = 0;
+ 	timer.it_value.tv_usec = 1000;
+
+ 	/* ... and every 1 sec after that. */
+ 	timer.it_interval.tv_sec = 0;
+ 	timer.it_interval.tv_usec = 1000;
+
+ 	/* Start a virtual timer. It counts down whenever this process is    executing. */
+ 	setitimer (ITIMER_VIRTUAL, &timer, NULL);
 
 	/* creating socket stream */
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
@@ -97,9 +152,8 @@ int main(int argc, char*argv[])
 		exit(1);
 	}
 
-
 	/* registering the signal handler */
-	if (signal(SIGINT, my_handler_for_sigint) == SIG_ERR)
+	if (signal(SIGINT, periodic_task) == SIG_ERR)
       		printf("\ncan't catch SIGINT\n");
 
     	/* create a new playback stream */
@@ -111,7 +165,6 @@ int main(int argc, char*argv[])
 
 	for(;;) 
 	{
-		int n;
 		printf("Waiting for a connection\n");
 
 		/* accept connection fron client */
@@ -124,34 +177,6 @@ int main(int argc, char*argv[])
 	
 		for(;;)
 		{
-		
-        		uint8_t bufs[BUFSIZE];
-        		ssize_t r;
-
-			#if 0
-        		pa_usec_t latency;
-
-        		if ((latency = pa_simple_get_latency(s, &error)) == (pa_usec_t) -1) {
-            			fprintf(stderr, __FILE__": pa_simple_get_latency() failed: %s\n", pa_strerror(error));
-            			goto finish;
-        		}
-
-        		fprintf(stderr, "%0.0f usec    \r", (float)latency);
-			#endif
-
-			/* receiving message from client */
-			n = recv(s2, bufs, sizeof(bufs), 0);
-			if(n == -1)
-			{
-        			perror("recv");
-       				exit(1);
-			}
-
-        		if (pa_simple_write(s, bufs, sizeof(bufs), &error) < 0) 
-			{
-            			fprintf(stderr, __FILE__": pa_simple_write() failed: %s\n", pa_strerror(error));
-            			goto finish;
-        		}
 		}
 	}
 
