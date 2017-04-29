ee06911f1ba86c03182d83744156c5155388fedd
Prajwal Sharma K <prajwalsharmakote@gmail.com>
2017-04-29 22:06:58 +0530

    RTP Project along with G711

--- g711.c
+++ g711.c
@@ -29,29 +29,14 @@
  *
  * u-law, A-law and linear PCM conversions.
  */
-
-//#include "snack.h"
-
-/*
- * Functions Snack_Lin2Alaw, Snack_Lin2Mulaw have been updated to correctly
- * convert unquantized 16 bit values.
- * Tables for direct u- to A-law and A- to u-law conversions have been
- * corrected.
- * Borge Lindberg, Center for PersonKommunikation, Aalborg University.
- * bli@cpk.auc.dk
- *
- */
- 
 #define	SIGN_BIT	(0x80)		/* Sign bit for a A-law byte. */
 #define	QUANT_MASK	(0xf)		/* Quantization field mask. */
 #define	NSEGS		(8)		/* Number of A-law segments. */
 #define	SEG_SHIFT	(4)		/* Left shift for segment number. */
 #define	SEG_MASK	(0x70)		/* Segment field mask. */
 
-static short seg_aend[8] = {0x1F, 0x3F, 0x7F, 0xFF,
-			    0x1FF, 0x3FF, 0x7FF, 0xFFF};
-static short seg_uend[8] = {0x3F, 0x7F, 0xFF, 0x1FF,
-			    0x3FF, 0x7FF, 0xFFF, 0x1FFF};
+static short seg_end[8] = {0xFF, 0x1FF, 0x3FF, 0x7FF,
+			    0xFFF, 0x1FFF, 0x3FFF, 0x7FFF};
 
 /* copy from CCITT G.711 specifications */
 unsigned char _u2a[128] = {			/* u- to A-law conversions */
@@ -65,10 +50,7 @@ unsigned char _u2a[128] = {			/* u- to A-law conversions */
 	55,	56,	57,	58,	59,	60,	61,	62,
 	64,	65,	66,	67,	68,	69,	70,	71,
 	72,	73,	74,	75,	76,	77,	78,	79,
-/* corrected:
-	81,	82,	83,	84,	85,	86,	87,	88, 
-   should be: */
-	80,	82,	83,	84,	85,	86,	87,	88,
+	81,	82,	83,	84,	85,	86,	87,	88,
 	89,	90,	91,	92,	93,	94,	95,	96,
 	97,	98,	99,	100,	101,	102,	103,	104,
 	105,	106,	107,	108,	109,	110,	111,	112,
@@ -85,11 +67,7 @@ unsigned char _a2u[128] = {			/* A- to u-law conversions */
 	50,	51,	52,	53,	54,	55,	56,	57,
 	58,	59,	60,	61,	62,	63,	64,	64,
 	65,	66,	67,	68,	69,	70,	71,	72,
-/* corrected:
 	73,	74,	75,	76,	77,	78,	79,	79,
-   should be: */
-	73,	74,	75,	76,	77,	78,	79,	80,
-
 	80,	81,	82,	83,	84,	85,	86,	87,
 	88,	89,	90,	91,	92,	93,	94,	95,
 	96,	97,	98,	99,	100,	101,	102,	103,
@@ -97,13 +75,13 @@ unsigned char _a2u[128] = {			/* A- to u-law conversions */
 	112,	113,	114,	115,	116,	117,	118,	119,
 	120,	121,	122,	123,	124,	125,	126,	127};
 
-static short
-search(
-	short		val,
-	short		*table,
-	short		size)
+static int
+search(val, table, size)
+	int		val;
+	short		*table;
+	int		size;
 {
-	short		i;
+	int		i;
 
 	for (i = 0; i < size; i++) {
 		if (val <= *table++)
@@ -112,10 +90,13 @@ search(
 	return (size);
 }
 
+/* see libst.h */
+//#ifdef	SUPERCEDED
+
 /*
- * Snack_Lin2Alaw() - Convert a 16-bit linear PCM value to 8-bit A-law
+ * linear2alaw() - Convert a 16-bit linear PCM value to 8-bit A-law
  *
- * Snack_Lin2Alaw() accepts an 16-bit integer and encodes it as A-law data.
+ * linear2alaw() accepts an 16-bit integer and encodes it as A-law data.
  *
  *		Linear Input Code	Compressed Code
  *	------------------------	---------------
@@ -131,51 +112,48 @@ search(
  * For further information see John C. Bellamy's Digital Telephony, 1982,
  * John Wiley & Sons, pps 98-111 and 472-476.
  */
-
 unsigned char
-Snack_Lin2Alaw(
-	short		pcm_val)	/* 2's complement (16-bit range) */
+linear2alaw(pcm_val)
+	int		pcm_val;	/* 2's complement (16-bit range) */
 {
-	short		mask;
-	short		seg;
+	int		mask;
+	int		seg;
 	unsigned char	aval;
-
-	pcm_val = pcm_val >> 3;
 
 	if (pcm_val >= 0) {
 		mask = 0xD5;		/* sign (7th) bit = 1 */
 	} else {
 		mask = 0x55;		/* sign bit = 0 */
-		pcm_val = -pcm_val - 1;
+		pcm_val = -pcm_val - 8;
 	}
 
 	/* Convert the scaled magnitude to segment number. */
-	seg = search(pcm_val, seg_aend, 8);
+	seg = search(pcm_val, seg_end, 8);
 
 	/* Combine the sign, segment, and quantization bits. */
 
 	if (seg >= 8)		/* out of range, return maximum value. */
-		return (unsigned char) (0x7F ^ mask);
+		return (0x7F ^ mask);
 	else {
-		aval = (unsigned char) seg << SEG_SHIFT;
+		aval = seg << SEG_SHIFT;
 		if (seg < 2)
-			aval |= (pcm_val >> 1) & QUANT_MASK;
+			aval |= (pcm_val >> 4) & QUANT_MASK;
 		else
-			aval |= (pcm_val >> seg) & QUANT_MASK;
+			aval |= (pcm_val >> (seg + 3)) & QUANT_MASK;
 		return (aval ^ mask);
 	}
 }
 
 /*
- * Snack_Alaw2Lin() - Convert an A-law value to 16-bit linear PCM
+ * alaw2linear() - Convert an A-law value to 16-bit linear PCM
  *
  */
-short
-Snack_Alaw2Lin(
-	unsigned char	a_val)
+int
+alaw2linear(a_val)
+	unsigned char	a_val;
 {
-	short		t;
-	short		seg;
+	int		t;
+	int		seg;
 
 	a_val ^= 0x55;
 
@@ -196,10 +174,9 @@ Snack_Alaw2Lin(
 }
 
 #define	BIAS		(0x84)		/* Bias for linear code. */
-#define CLIP            8159
 
 /*
- * Snack_Lin2Mulaw() - Convert a linear PCM value to u-law
+ * linear2ulaw() - Convert a linear PCM value to u-law
  *
  * In order to simplify the encoding process, the original linear magnitude
  * is biased by adding 33 which shifts the encoding range from (0 - 8158) to
@@ -228,42 +205,40 @@ Snack_Alaw2Lin(
  * John Wiley & Sons, pps 98-111 and 472-476.
  */
 unsigned char
-Snack_Lin2Mulaw(
-	short		pcm_val)	/* 2's complement (16-bit range) */
+linear2ulaw(pcm_val)
+	int		pcm_val;	/* 2's complement (16-bit range) */
 {
-	short		mask;
-	short		seg;
+	int		mask;
+	int		seg;
 	unsigned char	uval;
 
 	/* Get the sign and the magnitude of the value. */
-	pcm_val = pcm_val >> 2;
 	if (pcm_val < 0) {
-		pcm_val = -pcm_val;
+		pcm_val = BIAS - pcm_val;
 		mask = 0x7F;
 	} else {
+		pcm_val += BIAS;
 		mask = 0xFF;
 	}
-        if ( pcm_val > CLIP ) pcm_val = CLIP;		/* clip the magnitude */
-	pcm_val += (BIAS >> 2);
 
 	/* Convert the scaled magnitude to segment number. */
-	seg = search(pcm_val, seg_uend, 8);
+	seg = search(pcm_val, seg_end, 8);
 
 	/*
 	 * Combine the sign, segment, quantization bits;
 	 * and complement the code word.
 	 */
 	if (seg >= 8)		/* out of range, return maximum value. */
-		return (unsigned char) (0x7F ^ mask);
+		return (0x7F ^ mask);
 	else {
-		uval = (unsigned char) (seg << 4) | ((pcm_val >> (seg + 1)) & 0xF);
+		uval = (seg << 4) | ((pcm_val >> (seg + 3)) & 0xF);
 		return (uval ^ mask);
 	}
 
 }
 
 /*
- * Snack_Mulaw2Lin() - Convert a u-law value to 16-bit linear PCM
+ * ulaw2linear() - Convert a u-law value to 16-bit linear PCM
  *
  * First, a biased linear code is derived from the code word. An unbiased
  * output can then be obtained by subtracting 33 from the biased code.
@@ -271,11 +246,11 @@ Snack_Lin2Mulaw(
  * Note that this function expects to be passed the complement of the
  * original code word. This is in keeping with ISDN conventions.
  */
-short
-Snack_Mulaw2Lin(
-	unsigned char	u_val)
+int
+ulaw2linear(u_val)
+	unsigned char	u_val;
 {
-	short		t;
+	int		t;
 
 	/* Complement to obtain normal u-law value. */
 	u_val = ~u_val;
@@ -290,23 +265,24 @@ Snack_Mulaw2Lin(
 	return ((u_val & SIGN_BIT) ? (BIAS - t) : (t - BIAS));
 }
 
+//#endif
+
 /* A-law to u-law conversion */
 unsigned char
-alaw2ulaw(
-	unsigned char	aval)
+alaw2ulaw(aval)
+	unsigned char	aval;
 {
 	aval &= 0xff;
-	return (unsigned char) ((aval & 0x80) ? (0xFF ^ _a2u[aval ^ 0xD5]) :
+	return ((aval & 0x80) ? (0xFF ^ _a2u[aval ^ 0xD5]) :
 	    (0x7F ^ _a2u[aval ^ 0x55]));
 }
 
 /* u-law to A-law conversion */
 unsigned char
-ulaw2alaw(
-	unsigned char	uval)
+ulaw2alaw(uval)
+	unsigned char	uval;
 {
 	uval &= 0xff;
-	return (unsigned char) ((uval & 0x80) ? (0xD5 ^ (_u2a[0xFF ^ uval] - 1)) :
-	    (unsigned char) (0x55 ^ (_u2a[0x7F ^ uval] - 1)));
+	return ((uval & 0x80) ? (0xD5 ^ (_u2a[0xFF ^ uval] - 1)) :
+	    (0x55 ^ (_u2a[0x7F ^ uval] - 1)));
 }
-
